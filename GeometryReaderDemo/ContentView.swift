b60654e41b342b2777ae711e562fdf1050c0d4fd
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-31 20:50:34 -0600

    Finished.

--- GeometryReaderDemo/ContentView.swift
+++ GeometryReaderDemo/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  var body: some View {
+    
+    GeometryReader { geometry in
+      
+      VStack {
+        Rectangle()
+          .frame(width: geometry.size.width/4, height: geometry.size.height/8, alignment: .center)
+          .onTapGesture {
+            // Coordinates take safe area into account
+            print("Global: x: \(geometry.frame(in: .global).minX),y: \(geometry.frame(in: .global).minY)")
+            // Coordinates DO NOT take safe area into account
+            print("Local: x: \(geometry.frame(in: .local).minX),y: \(geometry.frame(in: .local).minY)")
+          }
+        
+        // Coordinates printed for this rectangle are the same as the rectangle above. This is because it is not displaying the coordinates of the green rectangle, it is displaying the coordinates of the GeometryReader! If you want the individual coordinates of each rectangle, they should both be wrapped in their own GeometryReader.
+        Rectangle()
+          .foregroundColor(.green)
+          .frame(width: geometry.size.width/4, height: geometry.size.height/8, alignment: .center)
+          .onTapGesture {
+            print("Global: x: \(geometry.frame(in: .global).minX),y: \(geometry.frame(in: .global).minY)")
+            print("Local: x: \(geometry.frame(in: .local).minX),y: \(geometry.frame(in: .local).minY)")
+          }
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }

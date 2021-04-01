0be1d2418b57bcc0eba2fdfb7dd4c488690c54e4
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-31 20:34:41 -0600

    Initial Commit

---
+++ GeometryReaderDemo/GeometryReaderDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderDemoApp.swift
+//  GeometryReaderDemo
+//
+//  Created by Fred Javalera on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
